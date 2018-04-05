8e4737f73fee68e64f83eb7d3964d3b631195905
Artsiom Grintsevich <greenfrvr@gmail.com>
2018-04-06 00:07:08 +0300

    Initial Commit

---
+++ StretchyTextView/StretchyTextView.h
@@ -0,0 +1,19 @@
+//
+//  StretchyTextView.h
+//  StretchyTextView
+//
+//  Created by Artsiom Grintsevich on 06.04.2018.
+//  Copyright © 2018 Artsiom Grintsevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StretchyTextView.
+FOUNDATION_EXPORT double StretchyTextViewVersionNumber;
+
+//! Project version string for StretchyTextView.
+FOUNDATION_EXPORT const unsigned char StretchyTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StretchyTextView/PublicHeader.h>
+
+
